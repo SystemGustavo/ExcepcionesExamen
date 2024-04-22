5c40ca2bc2edf33d4532af16d7c6be9dfc557b49
CESAR <CESAR@SYSTEMGUSTAVO>
2024-04-21 21:51:59 -0500

    pregunta 5 lista

---
+++ DivisionConValoresInvalidos/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static int DividirNumeros(int numero1, int numero2)
+    {
+        if (numero1 < 0 || numero2 < 0)
+        {
+            throw new ArgumentException();
+        }
+        return numero1 / numero2;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese el 1er Numero");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el 2do Numero");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+
+        try
+        {
+            int res = DividirNumeros(numero1, numero2);
+            Console.WriteLine($"\n{res}");
+        }
+        catch (ArgumentException)
+        {
+            Console.Write("Ambos números deben ser positivos para realizar la división");
+        }
+        finally
+        {
+            Console.ReadKey();
+        }
+    }
+}
